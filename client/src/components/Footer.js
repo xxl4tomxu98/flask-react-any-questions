2295e1b14c180a6d5743f2285398add8b9923cee
xxl4tomxu98 <tomxu@udel.edu>
2020-12-11 11:09:35 -0500

    fix resume1

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -58,7 +58,7 @@ const Footer = () => {
                                     <List.Item as='a' href="https://www.linkedin.com/in/tom-xu-dupont">LinkedIn</List.Item>
                                     <List.Item as='a' href="https://github.com/xxl4tomxu98">Github</List.Item>
                                     <List.Item as='a' href="https://xxl4tomxu98.github.io/">Website</List.Item>
-                                    <List.Item as='a' href="https://dochub.com/tomxu-vtgtez/Gd71aZOw4p3kZnAw2Q9AP3/tom-xu-resume-pdf">Resume</List.Item>
+                                    <List.Item as='a' href="https://dochub.com/tomxu-vtgtez/Gd71aZOw4p3kZnAw2Q9AP3/tom-xu-resume-pdf?gdiExists=1yZacZ5iyp3a_TB7xwl3UHp6bfOLEsJ2p">Resume</List.Item>
                                 </List>
                             </Grid.Column>
                         </Grid.Row>
